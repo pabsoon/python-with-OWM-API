da719b309298c0a720140d570e2c2b670b02db46
pabsoon <mariusz.pabiniak@wp.pl>
2022-09-26 15:42:49 +0200

    Added new element in gui (description, settings element etc). Added function to check if config file exists. Added function with popups.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 import owmAPI
@@ -12,9 +12,16 @@ class WeatherApp(QMainWindow):
         uic.loadUi("WeatherAppGui.ui", self)
 
         self.get_data_btn = self.findChild(QPushButton, "getDataBtn")
-        self.plain_text = self.findChild(QPlainTextEdit, "plainText")
         self.image_label = self.findChild(QLabel, "imageLabel")
         self.temp_label = self.findChild(QLabel, "tempLabel")
+        self.details_box = self.findChild(QGroupBox, "detailsBox")
+        self.description_label = self.findChild(QLabel, "descriptionLabel")
+        self.feels_like_label = self.findChild(QLabel, "feelsLikeLabel")
+        self.pressure_label = self.findChild(QLabel, "pressureLabel")
+        self.humidity_label = self.findChild(QLabel, "humidityLabel")
+        self.wind_label = self.findChild(QLabel, "windLabel")
+        self.clouds_label = self.findChild(QLabel, "cloudsLabel")
+        self.save_button = self.findChild(QPushButton, "saveButton")
 
         self.get_data_btn.clicked.connect(self.get_data)
 
@@ -22,12 +29,27 @@ class WeatherApp(QMainWindow):
         weather_response = owmAPI.get_weather_by_city_name(self, "Warsaw", "PL", "metric")
         weather_json = weather_response.json()
         code = weather_json["weather"][0]["icon"]
-        image = owmAPI.get_weather_image(self, code)
         temperature = weather_json["main"]["temp"]
+        country_name = weather_json["name"]
+        country_code = weather_json["sys"]["country"]
+        description = weather_json["weather"][0]["description"]
+        feels_like = weather_json["main"]["feels_like"]
+        pressure = weather_json["main"]["pressure"]
+        humidity = weather_json["main"]["humidity"]
+        wind = weather_json["wind"]["speed"]
+        clouds = weather_json["clouds"]["all"]
 
-        self.plain_text.setPlainText(weather_response.text)
+        image = owmAPI.get_weather_image(self, code)
+        self.details_box.setTitle(f"{country_name}, {country_code}")
         self.image_label.setPixmap(QPixmap(image))
         self.temp_label.setText(f"{temperature}°")
+        self.description_label.setText(f"{description}")
+        self.feels_like_label.setText(f"{feels_like}°")
+        self.pressure_label.setText(f"{pressure} hPa")
+        self.humidity_label.setText(f"{humidity} %")
+        self.wind_label.setText(f"{wind}")
+        self.clouds_label.setText(f"{clouds} %")
+
         pass
 
 
@@ -35,4 +57,5 @@ app = QApplication(sys.argv)
 
 window = WeatherApp()
 window.show()
+owmAPI.check_config(window)
 app.exec_()
--- owmAPI.py
+++ owmAPI.py
@@ -1,16 +1,17 @@
+import os
+
 import requests
 from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import QLineEdit, QTabWidget, QMessageBox
 
-import api_key
+import config
 
-
-# city_name = "Warsaw"
-# units = "metric"
+CONF_FILE = "config.py"
 
 
 def get_weather_by_city_name(self, city_name: str, country_code: str, units: str):
     weather_response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&appid={api_key.API_KEY}&units={units}")
+        f"https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&appid={config.API_KEY}&units={units}")
     return weather_response
 
 
@@ -19,3 +20,37 @@ def get_weather_image(self, code: str):
     image = QImage()
     image.loadFromData(image_response.content)
     return image
+
+
+def check_config(self):
+    self.line_edit_key = self.findChild(QLineEdit, "lineEditKey")
+    self.tab_widget = self.findChild(QTabWidget, "tabWidget")
+    if check_if_config_exists() is not True:
+        file = open(CONF_FILE, "a")
+        file.write("API_KEY = ''\nREFRESH_TIME = 5\nUNITS = 'metric'\n")
+    elif len(config.API_KEY) < 31:
+        self.tab_widget.setCurrentIndex(1)
+        show_pop_up("Warning", "Please paste your API Key in Settings Tab")
+    else:
+        # TODO timer
+        self.tab_widget.setCurrentIndex(0)
+
+
+def show_pop_up(title: str, text: str):
+    msg = QMessageBox()
+    msg.setWindowTitle(title)
+    msg.setText(text)
+    msg.exec_()
+
+
+def check_if_config_exists():
+    return os.path.isfile(CONF_FILE)
+
+
+def save_changed_config(self, refresh_time: int, api_key: str, units: str):
+    if refresh_time != config.REFRESH_TIME:
+        config.REFRESH_TIME = refresh_time
+    if api_key != config.API_KEY:
+        config.API_KEY = api_key
+    if units != config.UNITS:
+        config.UNITS = units
